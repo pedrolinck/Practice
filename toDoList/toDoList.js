fa9fe754907fd4d10e82e15da74058bf21c77ae3
pedro_linck@outlook.com <pedro_linck@outlook.com>
2024-09-18 20:59:13 -0300

    add logic to clear input after add a task

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -2,17 +2,37 @@
   "use strict"
 
   //store variables
-  const input = document.getElementById('input-item').value
-  const formList = document.getElementById('form')
-  const ul = document.getElementById('list')
+  const inputList = document.getElementById('input-item');
+  const formList = document.getElementById('form');
+  const ul = document.getElementById('list');
+  const lis = document.getElementsByTagName("li")
+
+  function addTask(task){
+    const li = document.createElement('li')
+    li.className = 'list-item'
+    const p = document.createElement('p')
+    p.className = 'teste'
+    p.textContent = task
+    li.appendChild(p)
+    ul.appendChild(li)
+  }
 
   formList.addEventListener('submit', function(e) {
     e.preventDefault()
-    ul.innerHTML += `
-      <li class="list-item">
-        <p id="teste">${input}</p>
-      </li>
-    `
-  })
+    // ul.innerHTML += `
+    //   <li class="list-item">
+    //     <p id="teste">${inputList.value}</p>
+    //   </li>
+    // `
+    
+    inputList.value = " ";
+    inputList.focus();
+  });
+
+  [...lis].forEach(element => {
+    element.addEventListener('click', function (e){
+      console.log(this)
+    })
+  });
 
 })()
